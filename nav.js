c0a8aa443d4ce4bbad79ac4212b0b63d3e799a51
Sporule <hao@sporule.com>
2019-11-13 19:30:20 +0000

    merge conflicts

--- nav.js
+++ nav.js
@@ -20,16 +20,10 @@ const NavTemplate = () => {
                             })
                         }
                     </ul>
-<<<<<<< HEAD
                     <form role="search" method="get" className="searchBox" action="/posts/">
                     <input type="hidden" placeholder="Search Here" name="pagetype" value="search" onChange={o => { }}  readOnly/> 
                     <input type="text" placeholder="Search Here" name="search" onChange={o => { }}  /> 
                     
-=======
- <form role="search" method="get" className="searchBox" action="/posts/">
-                    <input type="hidden" placeholder="Search Here" name="pagetype" value="search" onChange={o => { }}  readOnly/> 
-                    <input type="text" placeholder="Search Here" name="search" onChange={o => { }}  /> 
->>>>>>> upstream/master
                     </form>
                 </nav>
             </div>
